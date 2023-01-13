c029d49ff00240af972298dd834b13c9c0633be5
pinky-pig <2545589993@qq.com>
2023-01-13 17:05:09 +0800

    feat: setting - background

--- src/newtab/layout/drawer/Bg.vue
+++ src/newtab/layout/drawer/Bg.vue
@@ -2,6 +2,37 @@
 import { SETTINGS } from './settings'
 
 const config = SETTINGS.filter(i => i.name === 'Background')[0]
+
+type BgType = 'random-colors' | 'linear-colors' | 'image'
+interface BackgroundMode {
+  type: BgType
+  value: string
+  name: string
+}
+const mode = ref<BackgroundMode[]>([
+  {
+    type: 'random-colors',
+    value: ' random',
+    name: '随机颜色',
+  },
+  {
+    type: 'linear-colors',
+    value: 'rgba(0,0,0,)',
+    name: '线性渐变',
+
+  },
+  {
+    type: 'image',
+    value: 'https://',
+    name: '图片',
+  },
+])
+
+const currentMode = ref<BgType>('random-colors')
+
+function handleSwitchBgMode(item: BackgroundMode) {
+  currentMode.value = item.type
+}
 </script>
 
 <template>
@@ -16,7 +47,24 @@ const config = SETTINGS.filter(i => i.name === 'Background')[0]
     </div>
     <n-divider />
     <!-- body -->
-    <div class="h-500px rounded" />
+    <div class="h-500px rounded">
+      <!-- mode -->
+      <div style="flex:0 0 auto" class="p-3px h-90px rounded flex flex-row justify-between bg-[var(--primary-bg-color)] gap-2">
+        <div
+          v-for="item in mode"
+          :key="item.type"
+          :style="{ background: currentMode === item.type ? 'var(--second-bg-color )' : 'var(--primary-bg-color)' }"
+          class=" mode-item cursor-pointer w-1/3 h-full rounded box-border p-6px flex justify-between flex-col"
+          @click="handleSwitchBgMode(item)"
+        >
+          <div v-if="item.type === 'random-colors'" class="text-20px" i-material-symbols:brush />
+          <div v-if="item.type === 'linear-colors'" class="text-20px" i-material-symbols:broken-image />
+          <div v-if="item.type === 'image'" class="text-20px" i-ic:baseline-blur-linear />
+
+          {{ item.name }}
+        </div>
+      </div>
+    </div>
   </n-card>
 </template>
 
@@ -26,5 +74,12 @@ const config = SETTINGS.filter(i => i.name === 'Background')[0]
   padding: 10px;
   margin-top: 10px;
   margin-bottom: 10px;
+}
+
+.mode-item{
+  transition: 0.2s all ease-in-out;
+}
+.mode-item:hover{
+  background-color: var(--second-bg-color) !important;
 }
 </style>
