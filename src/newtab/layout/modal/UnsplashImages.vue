9ecb5dbe113a1d1c131ac367723ffbac3bcfad92
pinky-pig <2545589993@qq.com>
2023-01-20 15:26:36 +0800

    fix: 修改unsplash的检索

--- src/newtab/layout/modal/UnsplashImages.vue
+++ src/newtab/layout/modal/UnsplashImages.vue
@@ -14,6 +14,7 @@ const unsplash = createApi({
 const pageNum = ref(1)
 const pageSize = ref(9)
 const resultTotal = ref(9)
+const searchInputText = ref('')
 
 const imageList = ref<IBasic[]>([])
 watch(pageNum, () => {
@@ -22,48 +23,50 @@ watch(pageNum, () => {
 
 getList()
 function getList() {
-  unsplash.photos.list({
-    page: pageNum.value,
-    perPage: pageSize.value,
-  }).then((res) => {
-    if (res.status === 200 && res.type === 'success') {
-      imageList.value = res.response.results
-      resultTotal.value = res.response.total
-    }
-  })
+  if (searchInputText.value) {
+    unsplash.search.getPhotos({
+      query: searchInputText.value,
+      page: pageNum.value,
+      perPage: pageSize.value,
+    }).then((res) => {
+      if (res.status === 200 && res.type === 'success') {
+        resultTotal.value = res.response.total
+        imageList.value = res.response.results
+      }
+    })
+  }
+  else {
+    unsplash.photos.list({
+      page: pageNum.value,
+      perPage: pageSize.value,
+    }).then((res) => {
+      if (res.status === 200 && res.type === 'success') {
+        imageList.value = res.response.results
+        resultTotal.value = res.response.total
+      }
+    })
+  }
 }
 
-const searchInputText = ref('')
 function selectInternetImage(item: IBasic) {
   emit('handleSetInternetImage', item.urls.regular)
 }
 
-function handleSearchUnsplashImage() {
-  unsplash.search.getPhotos({
-    query: searchInputText.value,
-    page: pageNum.value,
-    perPage: pageSize.value,
-  }).then((res) => {
-    if (res.status === 200 && res.type === 'success')
-      imageList.value = res.response.results
-  })
-}
-
 const pageCount = computed(() => {
-  return resultTotal.value / 9
+  return Math.ceil(resultTotal.value / 9)
 })
 </script>
 
 <template>
   <n-card
-    style="width: 60%;height: 600px; padding: 10px;min-width: 750px; "
+    style="width: 750px;height: 600px; padding: 10px; "
     :bordered="false"
     size="huge"
     role="dialog"
     aria-modal="true"
   >
     <div class=" flex flex-col gap-4 overflow-auto">
-      <n-input v-model:value="searchInputText" type="text" placeholder="检索壁纸" @keydown.enter="handleSearchUnsplashImage" />
+      <n-input v-model:value="searchInputText" type="text" placeholder="检索壁纸" @keydown.enter="getList" />
 
       <div class=" flex flex-row flex-wrap gap-10px justify-evenly">
         <div v-for="item in imageList" :key="item.id" class="w-200px h-150px border cursor-pointer" @dblclick="selectInternetImage(item)">
