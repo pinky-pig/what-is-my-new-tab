ed938d240c89f17a2f0b0c25e3151df6a4c29a99
pinky-pig <2545589993@qq.com>
2023-02-08 11:42:09 +0800

    feat: 新增defineCustomElement定义box

--- src/context-menu/littleWindow.ts
+++ src/context-menu/littleWindow.ts
@@ -1,13 +1,16 @@
 import { useMagicKeys } from '@vueuse/core'
+import type { StyleValue } from 'vue'
+import { defineCustomElement, h } from 'vue'
 
-/* eslint-disable no-console */
 export const watchContextMenuEvent = () => {
   let events: any = []
+  let elements: any
   browser.runtime.onMessage.addListener(
     (e) => {
       if (e.cmd === 'start') {
-        console.log(e)
-        events = addMouseEvent()
+        const { e, ele } = addMouseEvent()
+        events = e
+        elements = ele
       }
     },
   )
@@ -16,17 +19,57 @@ export const watchContextMenuEvent = () => {
   const { Escape } = useMagicKeys()
   watch(Escape, (v) => {
     if (v)
-      removeMouseEvent(events)
+      removeMouseEvent(events, elements)
   })
 }
 
+// 1. 定义选择框，框子尺寸位置，根据bounds控制
+const selectBoxBounds = ref([0, 0, 0, 0]) // x,y,width,height
+const SelectBoxElement = defineCustomElement({
+  setup() {
+    const getStyle = computed(() => {
+      return {
+        position: 'fixed',
+        left: `${selectBoxBounds.value[0]}px`,
+        top: `${selectBoxBounds.value[1]}px`,
+        width: `${selectBoxBounds.value[2]}px`,
+        height: `${selectBoxBounds.value[3]}px`,
+        background: '#ffffff',
+        outline: '1px solid #000000',
+        zIndex: 99999,
+      } as StyleValue
+    })
+    return () => {
+      return h('div', {
+        style: getStyle.value,
+        id: 'selectBoxElement',
+      })
+    }
+  },
+})
+// 放置重复多次创建
+if (!customElements.get('select-box-element'))
+  customElements.define('select-box-element', SelectBoxElement)
+
+function createBoxComponent() {
+  const boxElement = new SelectBoxElement()
+  window.document.body.appendChild(
+    boxElement,
+  )
+  return boxElement
+}
 function addMouseEvent() {
+  const boxElement = createBoxComponent()
+
   function mousedown(_e: MouseEvent) {
   }
   function mouseup(_e: MouseEvent) {
   }
-  function mousemove(_e: MouseEvent) {
-    console.log('move')
+  function mousemove(e: MouseEvent) {
+    if (e.target) {
+      const { x, y, width, height } = (e.target as HTMLElement).getBoundingClientRect()
+      selectBoxBounds.value = [x, y, width, height]
+    }
   }
   function mouseover(_e: MouseEvent) {
   }
@@ -42,11 +85,18 @@ function addMouseEvent() {
   window.addEventListener('wheel', wheel, false)
   window.addEventListener('click', click, false)
 
-  return [mousedown, mouseup, mousemove, mouseover, wheel, click]
+  return {
+    e: [mousedown, mouseup, mousemove, mouseover, wheel, click],
+    ele: boxElement,
+  }
 }
 
-function removeMouseEvent(events: any[]) {
+function removeMouseEvent(events: any[], elements: any) {
   events?.length > 0 && events.forEach((event) => {
     window.removeEventListener(event.name, event)
   })
+
+  window.document.body.removeChild(
+    elements,
+  )
 }
