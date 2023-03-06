26a846cf29bb27823c1d514c33738de0f6ebb2f3
pinky-pig <2545589993@qq.com>
2023-03-06 16:56:41 +0800

    fix: 使用v3-dragblock

--- src/newtab/layout/homepage-custom/index.vue
+++ src/newtab/layout/homepage-custom/index.vue
@@ -1,33 +1,74 @@
 <script setup lang="ts">
-import type { Ref } from 'vue'
-import { initGridContainer } from './GridContainer'
-import { getAllGridCellSortByIndex } from './gridCellData'
-import BoundsSVGContainer from './BoundsSVGContainer.vue'
-import GridCell from './GridCell.vue'
-import { useLayoutStore } from '~/store'
+import { markRaw, ref } from 'vue'
+import V3Dragblock from 'v3-dragblock'
+import GridCellOne from './GridCellOne.vue'
+// import { useLayoutStore } from '~/store';
+// import { editAGridCell, getAllGridCellSortByIndex } from './gridCellData';
+// import { useDebounceFn } from '@vueuse/core';
 
-const attachedLine: Ref<{ l: any[]; mv: any[];r: any[];t: any[];mh: any[];b: any[] } > = ref({ l: [], mv: [], r: [], t: [], mh: [], b: [] })
-const currentClickedElement: Ref<any> = ref()
-const GridContainer = initGridContainer(currentClickedElement, attachedLine)
+const gridCells = ref([
+  { id: '0', index: 0, x: 20, y: 35, width: 190, height: 110, component: markRaw(GridCellOne ) }, // 辫子
+  { id: '1', index: 0, x: 465, y: 25, width: 170, height: 160, component: markRaw(GridCellOne) }, // 摆手
+  { id: '2', index: 0, x: 385, y: 265, width: 200, height: 50, component: markRaw(GridCellOne) }, // text
+  { id: '3', index: 0, x: 150, y: 195, width: 130, height: 130, component: markRaw(GridCellOne) }, // 白边
+])
 
-const store = useLayoutStore()
-
-// 1.indexDB中查询数据
-async function initGridCell() {
-  store.gridCells = await getAllGridCellSortByIndex()
+const adsorbLineStyle = {
+  stroke: 'black',
+  fill: 'black',
+  strokeWidth: 2,
 }
-await initGridCell()
+
+// const store = useLayoutStore()
+// 1.indexDB中查询数据
+// async function initGridCell() {
+//   store.gridCells = await getAllGridCellSortByIndex()
+// }
+// await initGridCell()
+// console.log(store.gridCells,'initGridCell');
+
+
+const print = (val: string, e: any) => {
+
+}
+
+const save = (val: string, e: any) => {
+  console.log(val, e)
+}
+
+// const saveCanvasLayoutData = useDebounceFn(() => {
+//   store.gridCells.forEach((cell, index) => {
+//     // 存储一个index，是为了将其层级存储一下，越大越顶层
+//     editAGridCell({ ...cell, index })
+//   })
+// }, 1000)
+
 </script>
 
 <template>
-  <div>
-    <GridContainer>
-      <GridCell v-for="item, index in store.gridCells" :key="item?.id" v-model="store.gridCells[index]" />
-    </GridContainer>
-    <BoundsSVGContainer v-model="currentClickedElement" :current-clicked-element="currentClickedElement" :attached-line="attachedLine" />
-  </div>
+  <V3Dragblock
+    class="V3Dragblock"
+    :grid-cells="gridCells"
+    :activated="true"
+    :draggable="true"
+    :resizable="true"
+    :adsorbable="true"
+    :adsorb-line-style="adsorbLineStyle"
+    @dragging="print('dragging', $event)"
+    @drag-start="print('drag-start', $event)"
+    @drag-end="save('drag-end', $event)"
+    @resizing="print('resizing', $event)"
+    @resize-start="print('resize-start', $event)"
+    @resize-end="save('resize-end', $event)"
+  />
 </template>
 
 <style scoped>
-
+.V3Dragblock{
+  width: 100vw;
+  height: 100vh;
+  background: transparent;
+  position: relative;
+  overflow: hidden;
+}
 </style>
